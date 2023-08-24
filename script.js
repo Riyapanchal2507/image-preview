a813063dee8edfa741eac100594df652d62136dd
Riya Panchal <113497187+Riyapanchal2507@users.noreply.github.com>
2023-08-24 13:55:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imgPreview");
+const previewImage = previewContainer.querySelector(".img-preview__image");
+const previewDefaultText = previewContainer.querySelector(".img-preview__default-text");
+
+inpFile.addEventListener("change",function(){
+    const file = this.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+        
+        reader.addEventListener("load", function() {
+            previewImage.setAttribute("src", this.result);
+        });
+        reader.readAsDataURL(file);
+    }else{
+        previewDefaultText.style.display = "null";
+        previewImage.style.display = "null";
+        previewImage.setAttribute("src", "");
+    }
+});
